a69426d1cb78df28718420c82fb4b6cff4af826f
Dominik Eitner <dominikeitner@gmail.com>
2021-08-26 16:58:11 +0200

    Adding linter

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 const add = function (a,b) {
-    return a+b;
+    if(isNaN(a) || isNaN(b))
+    {
+        return 'Not a Number';
+    }
+    else{
+        return a+b;
+    }
 };
 
-function addi() {
-    let first = document.getElementById("adder1").value;
-    let second = document.getElementById("adder2").value;
-    if(isNaN(first) && isNaN(second))
+const sub = function (a,b) {
+    if(isNaN(a) || isNaN(b))
     {
-        alert( "Not a number" );
-        return false;
+        return 'Not a Number';
     }
-    else
-    {
-        var answer =add(parseFloat(first),parseFloat(second));
-        var result = document.getElementById('res');
-        result.value=answer;
+    else{
+        return a-b;
     }
+};
+function addi(){
+    let first = document.getElementById('num1').value;
+    let second = document.getElementById('num2').value;
+    var answer =add(parseFloat(first),parseFloat(second));
+    var result = document.getElementById('res');
+    result.value=answer;
 }
 
-module.exports = add;
+function subi() {
+    let first = document.getElementById('num1').value;
+    let second = document.getElementById('num2').value;
+    var answer =sub(parseFloat(first),parseFloat(second));
+    var result = document.getElementById('res');
+    result.value=answer;
+}
+
+
+module.exports = {add,sub};
--- main.test.js
+++ main.test.js
@@ -1,6 +1,11 @@
-const add = require('./main');
+const {add,sub} = require('./main');
 
 test('should output 2+3=5',()=>{
    const testtext = add(2,3);
    expect(testtext).toBe(5);
+});
+
+test('should output 3-2=1',()=>{
+   const testtext = sub(3,2);
+   expect(testtext).toBe(1);
 });
