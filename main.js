18f8cc3fb5c0a7fdc6f1b0d152cc740c6089a4b5
Dominik Eitner <dominikeitner@gmail.com>
2021-08-26 17:47:37 +0200

    Fix linting

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-const add = function (a,b) {
-    if(isNaN(a) || isNaN(b))
-    {
-        return 'Not a Number';
-    }
-    else{
-        return a+b;
-    }
+const add = function (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return 'Not a Number';
+  } else {
+    return a + b;
+  }
 };
 
-const sub = function (a,b) {
-    if(isNaN(a) || isNaN(b))
-    {
-        return 'Not a Number';
-    }
-    else{
-        return a-b;
-    }
+const sub = function (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return 'Not a Number';
+  } else {
+    return a - b;
+  }
 };
-function addi(){
-    let first = document.getElementById('num1').value;
-    let second = document.getElementById('num2').value;
-    var answer =add(parseFloat(first),parseFloat(second));
-    var result = document.getElementById('res');
-    result.value=answer;
+
+function addi () {
+  let first = document.getElementById('num1').value;
+  let second = document.getElementById('num2').value;
+  var answer = add(parseFloat(first), parseFloat(second));
+  var result = document.getElementById('res');
+  result.value = answer;
 }
 
-function subi() {
-    let first = document.getElementById('num1').value;
-    let second = document.getElementById('num2').value;
-    var answer =sub(parseFloat(first),parseFloat(second));
-    var result = document.getElementById('res');
-    result.value=answer;
+function subi () {
+  let first = document.getElementById('num1').value;
+  let second = document.getElementById('num2').value;
+  var answer = sub(parseFloat(first), parseFloat(second));
+  var result = document.getElementById('res');
+  result.value = answer;
 }
 
-
-module.exports = {add,sub};
+module.exports = { add, sub };
